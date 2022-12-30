c5f179c13f1d0445168891a1cd0181c2842a6295
anders.bjorkland <anders.bjorkland@kreationsbyran.se>
2022-12-30 10:17:36 +0100

    Add authorization code to dependency injection container

--- public/index.php
+++ public/index.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Database\Manager\AccessTokenManager;
+use App\Database\Manager\AuthorizationCodeManager;
 use App\Database\Manager\UserManager;
 use App\Database\Repository\AccessTokenRepository;
+use App\Database\Repository\AuthorizationCodeRepository;
 use App\Database\Repository\ClientRepository;
 use App\Database\Repository\ScopeRepository;
 use App\Database\Repository\UserRepository;
@@ -29,8 +31,22 @@ $container = new Container([
     UserRepository::class => fn(ConnectionInterface $connection) => new UserRepository($connection),
     ScopeRepository::class => fn(ConnectionInterface $connection) => new ScopeRepository($connection),
     ClientRepository::class => fn(ConnectionInterface $connection) => new ClientRepository($connection),
-    AccessTokenManager::class => fn(ConnectionInterface $connection, ClientRepository $clientRepository) => new AccessTokenManager($connection, $clientRepository),
-    AccessTokenRepository::class => fn(AccessTokenManager $accessTokenManager) => new AccessTokenRepository($accessTokenManager),
+    AccessTokenManager::class => fn(
+        ConnectionInterface $connection, 
+        ClientRepository $clientRepository
+    ) => new AccessTokenManager($connection, $clientRepository),
+    AccessTokenRepository::class => fn (
+        AccessTokenManager $accessTokenManager
+    ) => new AccessTokenRepository($accessTokenManager),
+    AuthorizationCodeManager::class => 
+        fn (
+            ConnectionInterface $connection, 
+            ScopeRepository $scopeRepository, 
+            ClientRepository $clientRepository
+        ) => new AuthorizationCodeManager($connection, $scopeRepository, $clientRepository),
+    AuthorizationCodeRepository::class => fn (
+        AuthorizationCodeManager $authorizationCodeManager
+    ) => new AuthorizationCodeRepository($authorizationCodeManager),
     AuthorizationServer::class => function (
         ClientRepository $clientRepository, 
         ScopeRepository $scopeRepository, 
@@ -45,7 +61,8 @@ $container = new Container([
         );
         
         return $server;
-    },
+    }
+    
 ]);
 
 $app = new App($container);
